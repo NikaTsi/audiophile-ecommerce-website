f56bdeeb4133d15cdf1785e004a5e2ecd955a893
NikaTsi <nikawiklauri54@gmail.com>
2024-10-15 23:12:05 +0400

    updated product.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,21 +5,24 @@ import logo from '../assets/navigation/logo.svg';
 import cartIcon from '../assets/navigation/cart.svg';
 import Categories from './categories';
 import CartBox from './cart';
+import { useCartContext } from '../CartContext';
 
 export default function Header() {
     const [menu, setMenu] = useState(false);
-    const [cart, setCart] = useState(false);
+    const [cartVisible, setCartVisible] = useState(false);
     const [showHeader, setShowHeader] = useState(true);
     const [lastScrollY, setLastScrollY] = useState(0);
     const location = useLocation();
 
+    const { cart } = useCartContext();
+
     function handleMenu() {
         setMenu(!menu);
-        setCart(false);
+        setCartVisible(false);
     }
 
     function handleCart() {
-        setCart(!cart);
+        setCartVisible(!cartVisible);
         setMenu(false);
     }
 
@@ -44,11 +47,11 @@ export default function Header() {
 
     useEffect(() => {
         setMenu(false);
-        setCart(false);
+        setCartVisible(false);
     }, [location]);
 
     return (
-        <header className={`flex h-[91px] flex-col w-full font-Manrope bg-[#000000] sticky top-0 z-20 transition-transform duration-300 ${showHeader || menu || cart ? 'transform translate-y-0' : 'transform -translate-y-full'}`}>
+        <header className={`flex h-[91px] flex-col w-full font-Manrope bg-[#000000] sticky top-0 z-20 transition-transform duration-300 ${showHeader || menu || cartVisible ? 'transform translate-y-0' : 'transform -translate-y-full'}`}>
             <div className='flex w-full justify-between items-center py-8 px-6 md:py-8 md:px-10'>
                 <div className='flex w-full items-center gap-[76px] md:gap-[42px]'>
                     <img className='w-4 h-[15px] cursor-pointer' onClick={handleMenu} src={burgerMenu} alt="burgerMenu" />
@@ -60,12 +63,16 @@ export default function Header() {
                 <div></div>
 
                 <div className='flex flex-col items-center justify-center relative'>
-                    <div onClick={handleCart} className='absolute flex justify-center items-center w-[16px] h-[16px] bg-[#d87d4a] rounded-[50%] top-[-6px] left-[12px] cursor-pointer'><h1 className='text-[#FFFFFF] text-[12px]'>1</h1></div>
+                    {cart.length > 0 && (
+                        <div onClick={handleCart} className='absolute flex justify-center items-center w-[16px] h-[16px] bg-[#d87d4a] rounded-[50%] top-[-6px] left-[12px] cursor-pointer animate-bounce'>
+                            <h1 className='text-[#FFFFFF] text-[12px]'>{cart.length}</h1>
+                        </div>
+                    )}
                     <img onClick={handleCart} className='w-[24px] h-5 cursor-pointer' src={cartIcon} alt="cart" />
                 </div>
             </div>
 
-            {cart && <CartBox />}
+            {cartVisible && <CartBox />}
 
             <div className='flex w-full border-t border-[#fafafa] opacity-10 md:px-10'></div>
 
@@ -73,7 +80,7 @@ export default function Header() {
                 <Categories ptValue={true} />
             </div>
 
-            <div className={`flex w-full min-h-[100vh] bg-[#000] opacity-60 ${menu || cart ? "flex" : "hidden"}`}></div>
+            <div className={`flex w-full min-h-[100vh] bg-[#000] opacity-60 ${menu || cartVisible ? "flex" : "hidden"}`}></div>
         </header>
     );
 }
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -39,17 +39,17 @@ function Product() {
 
             <div className='flex flex-col w-full px-6 md:px-10'>
                 <div>
-                    <Link to={`/${category}`} className=' inline-block font-medium text-[15px] leading-[25px] text-[#000] opacity-50 pt-4 pb-6'>Go Back</Link>
+                    <Link to={`/${category}`} className=' inline-block font-medium text-[15px] leading-[25px] text-[#000] opacity-50 pt-4 pb-6 md:pt-8 md:pb-6'>Go Back</Link>
                 </div>
 
                 {product && (
-                    <div className='flex flex-col w-full gap-8 md:flex-row'>
-                        <div className='flex w-full justify-center pt-[56px] h-[327px] bg-[#f1f1f1] rounded-[8px]'>
+                    <div className='flex flex-col w-full gap-8 md:flex-row md:gap-[70px]'>
+                        <div className='flex w-full justify-center pt-[56px] h-[327px] bg-[#f1f1f1] rounded-[8px] md:h-[480px] md:pt-[133px]'>
                             <img className="h-[202px] drop-shadow-3xl" src={require(`../assets${product.img}`)} alt={product.title} />
                         </div>
 
-                        <div className='flex flex-col w-full gap-8'>
-                            <div className='flex flex-col gap-6'>
+                        <div className='flex flex-col w-full gap-8 md:my-[45px]'>
+                            <div className='flex flex-col gap-6 md:gap-8'>
                                 <span className='text-[14px] font-normal text-[#d87d4a] tracking-[10px]'>{product.description}</span>
                                 <h1 className='text-[28px] font-bold text-[#000] tracking-[1px] leading-[38px]'>{product.title}</h1>
                                 <h1 className='text-[15px] font-medium text-[#000] opacity-50 leading-[25px]'>{product.paragraph}</h1>
@@ -61,34 +61,36 @@ function Product() {
                                     <span className='font-bold text-[13px] tracking-[1px] text-[#000]'>{quantity}</span>
                                     <button className='flex items-center justify-center w-4 h-[18px]' onClick={incrementQuantity}>+</button>
                                 </div>
-                                <button className='font-bold w-40 h-[48px] bg-[#d87d4a] text-[13px] tracking-[1px] text-[#FFF]' onClick={handleAddToCart}>ADD TO CART</button>
+                                <button className='font-bold w-40 h-[48px] bg-[#d87d4a] text-[13px] tracking-[1px] text-[#FFF] hover:bg-[#fbaf85] duration-300 active:opacity-80' onClick={handleAddToCart}>ADD TO CART</button>
                             </div>
                         </div>
                     </div>
                 )}
 
-                {product && (<div>
-                    <div className='flex flex-col py-[88px] gap-6'>
-                        <h3 className='text-[24px] leading-[36px] font-bold text-[#000] tracking-[0.8px]'>FEATURES</h3>
-                        <p className='text-[15px] font-medium text-[#000] opacity-50 leading-[25px]'>{product.features}</p>
-                    </div>
-                    <div className='flex flex-col pb-[88px] gap-6'>
-                        <h3 className='text-[24px] leading-[36px] font-bold text-[#000] tracking-[0.8px]'>IN THE BOX</h3>
-                        <div className='flex flex-col gap-[8px]'>
-                            {product.box.amount.map((amount, index) => (
-                                <div className='flex gap-6' key={index}>
-                                    <span className='text-[15px] font-bold text-[#d87d4a] leading-[25px]'>{amount}</span>
-                                    <span className='text-[15px] font-medium text-[#000] opacity-50 leading-[25px]'>{product.box.items[index]}</span>
-                                </div>
-                            ))}
+                {product && (
+                    <div className='flex flex-col w-full'>
+                        <div className='flex flex-col py-[88px] gap-6 md:py-[120px]'>
+                            <h3 className='text-[24px] leading-[36px] font-bold text-[#000] tracking-[0.8px] md:text-[32px]'>FEATURES</h3>
+                            <p className='text-[15px] font-medium text-[#000] opacity-50 leading-[25px]'>{product.features}</p>
+                        </div>
+                        <div className='flex flex-col pb-[88px] gap-6 sm:flex-row sm:gap-[28%] md:pb-[120px]'>
+                            <h3 className='text-[24px] leading-[36px] font-bold text-[#000] tracking-[0.8px] md:text-[32px]'>IN THE BOX</h3>
+                            <div className='flex flex-col gap-[8px]'>
+                                {product.box.amount.map((amount, index) => (
+                                    <div className='flex gap-6' key={index}>
+                                        <span className='text-[15px] font-bold text-[#d87d4a] leading-[25px]'>{amount}</span>
+                                        <span className='text-[15px] font-medium text-[#000] opacity-50 leading-[25px]'>{product.box.items[index]}</span>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>)}
+                )}
 
-                <div className='flex flex-col w-full items-center gap-5'>
+                <div className='flex flex-col w-full items-center gap-5 md:grid md:grid-cols-2 md:grid-rows-2'>
                     {product && (
                         product.images.map((item, index) => (
-                            <div className='flex w-full items-center' key={index}>
+                            <div className={`flex w-full items-center ${index == 0 && "md:row-span-1 md:col-span-1"} ${index == 1 && "md:row-span-1 md:col-span-1 md:order-3"} ${index == 2 && "md:row-span-2 md:col-span-1 md:order-2"}`} key={index}> 
                                 <img className={`w-full rounded-[8px] object-cover ${imageHeight[index]}`} src={require(`../assets${item}`)} alt={product.title} />
                             </div>
                         ))
@@ -96,7 +98,7 @@ function Product() {
                 </div>
 
                 <div className='flex w-full justify-center mt-[120px]'>
-                    <h1 className='text-[26px] font-bold text-[#000] tracking-[0.9px] leading-[36px]'>YOU MAY ALSO LIKE</h1>
+                    <h1 className='text-[26px] font-bold text-[#000] tracking-[0.9px] leading-[36px] md:text-[32px]'>YOU MAY ALSO LIKE</h1>
                 </div>
 
                 {product && <OtherProducts product={product} />}
